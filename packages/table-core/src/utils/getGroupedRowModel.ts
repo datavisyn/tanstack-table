09428f8f306d6eee532f0be985a80ecba528604b
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2026-01-16 08:45:55 +0100

    Added new functions

--- packages/table-core/src/utils/getGroupedRowModel.ts
+++ packages/table-core/src/utils/getGroupedRowModel.ts
@@ -3,155 +3,132 @@ import { Row, RowData, RowModel, Table } from '../types'
 import { flattenBy, getMemoOptions, memo } from '../utils'
 import { GroupingState } from '../features/ColumnGrouping'
 
-export function getGroupedRowModel<TData extends RowData>(): (
-  table: Table<TData>
-) => () => RowModel<TData> {
-  return table =>
-    memo(
-      () => [table.getState().grouping, table.getPreGroupedRowModel()],
-      (grouping, rowModel) => {
-        if (!rowModel.rows.length || !grouping.length) {
-          // TODO: Does this have any consequences? Would avoid iterating the rows again
-          return rowModel;
-          /*
-          rowModel.rows.forEach(row => {
-            row.depth = 0
-            row.parentId = undefined
-          })
-          return rowModel
-          */
+export function getGroupedRowModelUnmemoized<TData extends RowData>(
+  table: Table<TData>,
+  grouping: GroupingState,
+  rowModel: RowModel<TData>
+): RowModel<TData> {
+  if (!rowModel.rows.length || !grouping.length) {
+    // TODO: Does this have any consequences? Would avoid iterating the rows again
+    return rowModel;
+    /*
+    rowModel.rows.forEach(row => {
+      row.depth = 0
+      row.parentId = undefined
+    })
+    return rowModel
+    */
+  }
+
+  // Filter the grouping list down to columns that exist
+  const existingGrouping = grouping.filter(columnId =>
+    table.getColumn(columnId)
+  )
+
+  const groupedFlatRows: Row<TData>[] = []
+  const groupedRowsById: Record<string, Row<TData>> = {}
+  // const onlyGroupedFlatRows: Row[] = [];
+  // const onlyGroupedRowsById: Record<RowId, Row> = {};
+  // const nonGroupedFlatRows: Row[] = [];
+  // const nonGroupedRowsById: Record<RowId, Row> = {};
+
+  // Recursively group the data
+  const groupUpRecursively = (
+    rows: Row<TData>[],
+    depth = 0,
+    parentId?: string
+  ) => {
+    // Grouping depth has been been met
+    // Stop grouping and simply rewrite thd depth and row relationships
+    if (depth >= existingGrouping.length) {
+      return rows.map(row => {
+        row.depth = depth
+
+        groupedFlatRows.push(row)
+        groupedRowsById[row.id] = row
+
+        if (row.subRows) {
+          row.subRows = groupUpRecursively(row.subRows, depth + 1, row.id)
         }
 
-        // Filter the grouping list down to columns that exist
-        const existingGrouping = grouping.filter(columnId =>
-          table.getColumn(columnId)
+        return row
+      })
+    }
+
+    const columnId: string = existingGrouping[depth]!
+
+    // Group the rows together for this level
+    const rowGroupsMap = groupBy(rows, columnId)
+
+    // Perform aggregations for each group
+    const aggregatedGroupedRows = Array.from(rowGroupsMap.entries()).map(
+      ([groupingValue, groupedRows], index) => {
+        let id = `${columnId}:${groupingValue}`
+        id = parentId ? `${parentId}>${id}` : id
+
+        // First, Recurse to group sub rows before aggregation
+        const subRows = groupUpRecursively(groupedRows, depth + 1, id)
+
+        subRows.forEach(subRow => {
+          subRow.parentId = id
+        })
+
+        // Flatten the leaf rows of the rows in this group
+        const leafRows = depth
+          ? flattenBy(groupedRows, row => row.subRows)
+          : groupedRows
+
+        const row = createRow(
+          table,
+          id,
+          leafRows[0]!.original,
+          index,
+          depth,
+          undefined,
+          parentId
         )
 
-        const groupedFlatRows: Row<TData>[] = []
-        const groupedRowsById: Record<string, Row<TData>> = {}
-        // const onlyGroupedFlatRows: Row[] = [];
-        // const onlyGroupedRowsById: Record<RowId, Row> = {};
-        // const nonGroupedFlatRows: Row[] = [];
-        // const nonGroupedRowsById: Record<RowId, Row> = {};
-
-        // Recursively group the data
-        const groupUpRecursively = (
-          rows: Row<TData>[],
-          depth = 0,
-          parentId?: string
-        ) => {
-          // Grouping depth has been been met
-          // Stop grouping and simply rewrite thd depth and row relationships
-          if (depth >= existingGrouping.length) {
-            return rows.map(row => {
-              row.depth = depth
-
-              groupedFlatRows.push(row)
-              groupedRowsById[row.id] = row
-
-              if (row.subRows) {
-                row.subRows = groupUpRecursively(row.subRows, depth + 1, row.id)
+        Object.assign(row, {
+          groupingColumnId: columnId,
+          groupingValue,
+          subRows,
+          leafRows,
+          getValue: (columnId: string) => {
+            // Don't aggregate columns that are in the grouping
+            if (existingGrouping.includes(columnId)) {
+              if (row._valuesCache.hasOwnProperty(columnId)) {
+                return row._valuesCache[columnId]
               }
 
-              return row
-            })
-          }
+              if (groupedRows[0]) {
+                row._valuesCache[columnId] =
+                  groupedRows[0].getValue(columnId) ?? undefined
+              }
 
-          const columnId: string = existingGrouping[depth]!
+              return row._valuesCache[columnId]
+            }
 
-          // Group the rows together for this level
-          const rowGroupsMap = groupBy(rows, columnId)
+            if (row._groupingValuesCache.hasOwnProperty(columnId)) {
+              return row._groupingValuesCache[columnId]
+            }
 
-          // Perform aggregations for each group
-          const aggregatedGroupedRows = Array.from(rowGroupsMap.entries()).map(
-            ([groupingValue, groupedRows], index) => {
-              let id = `${columnId}:${groupingValue}`
-              id = parentId ? `${parentId}>${id}` : id
+            // Aggregate the values
+            const column = table.getColumn(columnId)
+            const aggregateFn = column?.getAggregationFn()
 
-              // First, Recurse to group sub rows before aggregation
-              const subRows = groupUpRecursively(groupedRows, depth + 1, id)
-
-              subRows.forEach(subRow => {
-                subRow.parentId = id
-              })
-
-              // Flatten the leaf rows of the rows in this group
-              const leafRows = depth
-                ? flattenBy(groupedRows, row => row.subRows)
-                : groupedRows
-
-              const row = createRow(
-                table,
-                id,
-                leafRows[0]!.original,
-                index,
-                depth,
-                undefined,
-                parentId
+            if (aggregateFn) {
+              row._groupingValuesCache[columnId] = aggregateFn(
+                columnId,
+                leafRows,
+                groupedRows
               )
 
-              Object.assign(row, {
-                groupingColumnId: columnId,
-                groupingValue,
-                subRows,
-                leafRows,
-                getValue: (columnId: string) => {
-                  // Don't aggregate columns that are in the grouping
-                  if (existingGrouping.includes(columnId)) {
-                    if (row._valuesCache.hasOwnProperty(columnId)) {
-                      return row._valuesCache[columnId]
-                    }
-
-                    if (groupedRows[0]) {
-                      row._valuesCache[columnId] =
-                        groupedRows[0].getValue(columnId) ?? undefined
-                    }
-
-                    return row._valuesCache[columnId]
-                  }
-
-                  if (row._groupingValuesCache.hasOwnProperty(columnId)) {
-                    return row._groupingValuesCache[columnId]
-                  }
-
-                  // Aggregate the values
-                  const column = table.getColumn(columnId)
-                  const aggregateFn = column?.getAggregationFn()
-
-                  if (aggregateFn) {
-                    row._groupingValuesCache[columnId] = aggregateFn(
-                      columnId,
-                      leafRows,
-                      groupedRows
-                    )
-
-                    return row._groupingValuesCache[columnId]
-                  }
-                },
-              })
-
-              subRows.forEach(subRow => {
-                groupedFlatRows.push(subRow)
-                groupedRowsById[subRow.id] = subRow
-                // if (subRow.getIsGrouped?.()) {
-                //   onlyGroupedFlatRows.push(subRow);
-                //   onlyGroupedRowsById[subRow.id] = subRow;
-                // } else {
-                //   nonGroupedFlatRows.push(subRow);
-                //   nonGroupedRowsById[subRow.id] = subRow;
-                // }
-              })
-
-              return row
+              return row._groupingValuesCache[columnId]
             }
-          )
+          },
+        })
 
-          return aggregatedGroupedRows
-        }
-
-        const groupedRows = groupUpRecursively(rowModel.rows, 0)
-
-        groupedRows.forEach(subRow => {
+        subRows.forEach(subRow => {
           groupedFlatRows.push(subRow)
           groupedRowsById[subRow.id] = subRow
           // if (subRow.getIsGrouped?.()) {
@@ -163,11 +140,42 @@ export function getGroupedRowModel<TData extends RowData>(): (
           // }
         })
 
-        return {
-          rows: groupedRows,
-          flatRows: groupedFlatRows,
-          rowsById: groupedRowsById,
-        }
+        return row
+      }
+    )
+
+    return aggregatedGroupedRows
+  }
+
+  const groupedRows = groupUpRecursively(rowModel.rows, 0)
+
+  groupedRows.forEach(subRow => {
+    groupedFlatRows.push(subRow)
+    groupedRowsById[subRow.id] = subRow
+    // if (subRow.getIsGrouped?.()) {
+    //   onlyGroupedFlatRows.push(subRow);
+    //   onlyGroupedRowsById[subRow.id] = subRow;
+    // } else {
+    //   nonGroupedFlatRows.push(subRow);
+    //   nonGroupedRowsById[subRow.id] = subRow;
+    // }
+  })
+
+  return {
+    rows: groupedRows,
+    flatRows: groupedFlatRows,
+    rowsById: groupedRowsById,
+  }
+}
+
+export function getGroupedRowModel<TData extends RowData>(): (
+  table: Table<TData>
+) => () => RowModel<TData> {
+  return table =>
+    memo(
+      () => [table.getState().grouping, table.getPreGroupedRowModel()],
+      (grouping, rowModel) => {
+        return getGroupedRowModelUnmemoized(table, grouping, rowModel)
       },
       getMemoOptions(table.options, 'debugTable', 'getGroupedRowModel', () => {
         table._queue(() => {
